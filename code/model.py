c9f6ec304cec3f6a2738164379991b636cc6435d
Nick Cai <nickcai@pop-os.localdomain>
2021-11-29 02:05:40 -0800

    Added the components of AFGSANet.

---
+++ code/model.py
@@ -0,0 +1,106 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+from torch.utils import checkpoint
+
+# Conv2D + LeakyReLu + BatchNorm2D
+class Conv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding):
+        super(Conv, self).__init__()
+        conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=padding, padding_mode='reflect')
+        leaky_relu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        norm = nn.BatchNorm2d(out_channels, affine=True)
+        self.block = nn.Sequential(conv, leaky_relu, norm)
+
+    def forward(self, x):
+        return self.block(x)
+
+# Multi-scale Feature Extractor
+class MSFE(nn.Module):
+    def __init__(self, in_channels):
+        super(MSFE, self).__init__()
+        self.conv_1 = Conv(in_channels=in_channels, out_channels=256, kernel_size=1, padding=0)
+        self.conv_3 = Conv(in_channels=in_channels, out_channels=256, kernel_size=3, padding=1)
+        self.conv_5 = Conv(in_channels=in_channels, out_channels=256, kernel_size=5, padding=2)
+        self.conv_concat = Conv(in_channels=256*3, out_channels=256, kernel_size=1, padding=0)
+
+    def forward(self, x):
+        return self.conv_concat(torch.cat([
+            self.conv_1(x),
+            self.conv_3(x),
+            self.conv_5(x)]), dim=1)
+
+# Auxiliary Feature Guided Self-attention
+class AFGSA(nn.Module):
+    def __init__(self, ch, block_size=8, halo_size=3, num_heads=4, bias=False):
+        super(AFGSA, self).__init__()
+        self.block_size = block_size
+        self.halo_size = halo_size
+        self.num_heads = num_heads
+        self.head_ch = ch // num_heads
+        assert ch % num_heads == 0, "ch should be divided by # heads"
+
+        # relative positional embedding: row and column embedding each with dimension 1/2 head_ch
+        self.rel_h = nn.Parameter(torch.randn(1, block_size+2*halo_size, 1, self.head_ch//2), requires_grad=True)
+        self.rel_w = nn.Parameter(torch.randn(1, 1, block_size+2*halo_size, self.head_ch//2), requires_grad=True)
+
+        self.conv_map = Conv(ch*2, ch, kernel_size=1, padding=0)
+        self.q_conv = nn.Conv2d(ch, ch, kernel_size=1, bias=bias)
+        self.k_conv = nn.Conv2d(ch, ch, kernel_size=1, bias=bias)
+        self.v_conv = nn.Conv2d(ch, ch, kernel_size=1, bias=bias)
+
+        self.reset_parameters()
+
+    def forward(self, noisy, aux):
+        n_aux = self.conv_map(torch.cat([noisy, aux], dim=1))
+        b, c, h, w, block, halo, heads = *noisy.shape, self.block_size, self.halo_size, self.num_heads
+        assert h % block == 0 and w % block == 0, 'feature map dimensions must be divisible by the block size'
+
+        q = self.q_conv(n_aux)
+        q = rearrange(q, 'b c (h k1) (w k2) -> (b h w) (k1 k2) c', k1=block, k2=block)
+        q *= self.head_ch ** -0.5  # b*#blocks, flattened_query, c
+
+        k = self.k_conv(n_aux)
+        k = F.unfold(k, kernel_size=block+halo*2, stride=block, padding=halo)
+        k = rearrange(k, 'b (c a) l -> (b l) a c', c=c)
+
+        v = self.v_conv(noisy)
+        v = F.unfold(v, kernel_size=block+halo*2, stride=block, padding=halo)
+        v = rearrange(v, 'b (c a) l -> (b l) a c', c=c)
+
+        # b*#blocks*#heads, flattened_vector, head_ch
+        q, v = map(lambda i: rearrange(i, 'b a (h d) -> (b h) a d', h=heads), (q, v))
+        # positional embedding
+        k = rearrange(k, 'b (k1 k2) (h d) -> (b h) k1 k2 d', k1=block+2*halo, h=heads)
+        k_h, k_w = k.split(self.head_ch//2, dim=-1)
+        k = torch.cat([k_h+self.rel_h, k_w+self.rel_w], dim=-1)
+        k = rearrange(k, 'b k1 k2 d -> b (k1 k2) d')
+
+        # b*#blocks*#heads, flattened_query, flattened_neighborhood
+        sim = torch.einsum('b i d, b j d -> b i j', q, k)
+        attn = F.softmax(sim, dim=-1)
+        # b*#blocks*#heads, flattened_query, head_ch
+        out = torch.einsum('b i j, b j d -> b i d', attn, v)
+        out = rearrange(out, '(b h w n) (k1 k2) d -> b (n d) (h k1) (w k2)', b=b, h=(h//block), w=(w//block), k1=block, k2=block)
+        return out
+
+class Transformer(nn.Module):
+    def __init__(self, ch, block_size=8, halo_size=3, num_heads=4, checkpoint=True):
+        super(Transformer, self).__init__()
+        self.checkpoint = checkpoint
+        self.attention = AFGSA(ch, block_size=block_size, halo_size=halo_size, num_heads=num_heads)
+        self.feed_forward = nn.Sequential(
+            Conv(ch, ch, kernel_size=3, padding=1),
+            Conv(ch, ch, kernel_size=3, padding=1)
+        )
+
+    def forward(self, x):
+        if self.checkpoint:
+            noisy = x[0] + checkpoint(self.attention, x[0], x[1])
+        else:
+            noisy = x[0] + self.attention(x[0], x[1])
+        noisy = noisy + self.feed_forward(noisy)
+        return (noisy, x[1])
+
+
